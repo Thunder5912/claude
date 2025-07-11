a16afdae25352ddbb7bae90ce7778c12c71ff528
Thunder5912 <122851882+Thunder5912@users.noreply.github.com>
2025-07-11 12:55:57 +0530

    main.py

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ class TorrentDownloader:
             params = {
                 'save_path': download_path,
                 'storage_mode': lt.storage_mode_t(2),
-                "paused": True,
                 'auto_managed': True,
                 'duplicate_is_error': True
             }
